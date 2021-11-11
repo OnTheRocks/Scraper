54519852bc134ea81669c29989e778924e64f2fa
OnTheRocks <nathan_huber@yahoo.com>
2021-11-10 23:57:16 -0600

    Scraping

--- index.js
+++ index.js
@@ -8,5 +8,21 @@ const app = express()
 const url = 'https://www.theguardian.com/US'
 
 axios(url)
+  .then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const articles = []
+
+    $('.fc-item__title', html).each(function() {
+      const title = $(this).text()
+      const atitle = $(this).find('a').attr('href')
+      articles.push({
+        title,
+        atitle
+      })
+    })
+    console.log(articles)
+    .catch(err => console.log(err))
+  })
 
 app.listen(port, () => console.log("Server is running on PORT ${PORT}"))
